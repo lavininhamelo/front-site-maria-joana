66ed05f966ca702cb6863368b3885d05ebe86c31
Fernanda Cacho <flemes19@gmail.com>
2021-04-12 10:17:54 -0400

    Alteracao no arquivo abouther, no css

--- src/components/Abouther.vue
+++ src/components/Abouther.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="about container full-width column items-center">
         <Title title="Sobre" subtitle="nossa causa"></Title>
-        <div class="A artista">
+        <div class="Artista">
              <q-card class="my-card">
                 <q-card-section>
                     <div class="text-h6">A artista</div>
@@ -11,6 +11,30 @@
                 Atualmente está na Colômbia  para alavancar sua carreira Internacional, 
                 mas no momento o seu foco não está na parte artistica e sim em cursos 
                 e vendas de produtos em gerais...
+                </q-card-section>
+        </q-card>
+        </div>
+        <div class="LBGT">
+             <q-card class="my-card">
+                <q-card-section>
+                    <div class="text-h6">Movimento LGBT</div>
+                </q-card-section>
+                <q-card-section>
+               O movimento LGBT é uma organização sem fins lucrativos que promove a inclusão de pessoas LGBTI+ 
+               na sociedade com iniciativas de formação de lideranças, pesquisa, conscientização e segurança.
+                </q-card-section>
+        </q-card>
+        </div>
+        <div class="Veganismo">
+             <q-card class="my-card">
+                <q-card-section>
+                    <div class="text-h6">Veganismo</div>
+                </q-card-section>
+                <q-card-section>
+                O veganismo é a prática de se abster do uso de produtos de origem animal, procurando excluir, 
+                na medida do possível e praticável, o uso de qualquer produto de origem animal, 
+                seja na alimentação ou no vestuário. Um seguidor desta prática é conhecido como vegano, 
+                que é o caso de Maria Joana.
                 </q-card-section>
         </q-card>
         </div>
@@ -29,8 +53,13 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-   .about {
+  .about {
     background: #FFFFFF;
     }
+  .my-card{
+    background: #64743F;
+    color: #FFEDED;
+  }
+
 
 </style>
