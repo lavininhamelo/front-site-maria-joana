b2aa9e29bc5aa1cc0f241b7f99f328b984561a4f
Lavinia Melo <lavininhamelo@hotmail.com>
2021-04-16 22:45:18 -0300

    feat: req no product

--- src/pages/Products.vue
+++ src/pages/Products.vue
@@ -2,20 +2,36 @@
   <div class="w-full column items-center container q-pb-lg bg">
     <Title :title="'Produtos'" class="q-mt-lg" />
     <div class="row full-width justify-center q-my-lg">
-      <div class="q-px-lg " v-for="(card, index) in 30" :key="index">
-        <Card :name="`Produto Random ${index}`" :value="'R$ 23,00'"> </Card>
+      <div class="q-px-lg " v-for="(product, index) in products" :key="index">
+        <Card :name="product.name" :value="product.value"> </Card>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import axios from "axios";
 import Title from "src/components/global/Title.vue";
 import Card from "src/components/global/Card.vue";
 export default {
   components: {
     Title,
     Card
+  },
+  data() {
+    return {
+      products: []
+    };
+  },
+  methods: {
+    async getAllProducts() {
+      const response = await axios.post("http://localhost:3000/products");
+      console.log(response);
+      this.products = response.data;
+    }
+  },
+  mounted() {
+    this.getAllProducts();
   }
 };
 </script>
